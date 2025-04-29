e7d3e41b92a7a5b41052627ef992c992a0e29f64
Michael Wenzel <wenzel.michael@gmail.com>
2025-04-28 22:07:48 -0600

    prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-
+    <div className="min-h-svh min-w-svw flex flex-col items-center justify-center">
+      <Button>test</Button>
+    </div>
   );
 }
