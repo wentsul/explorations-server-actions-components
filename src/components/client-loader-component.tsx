59d8fadb43d56c0441d4a6f46daaca6aacd2836f
Michael Wenzel <wenzel.michael@gmail.com>
2025-04-30 10:45:53 -0600

    add error boundary

--- src/components/client-loader-component.tsx
+++ src/components/client-loader-component.tsx
@@ -3,6 +3,17 @@
 import { JSX, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { renderClientComponent } from "@/actions/server-action";
+import { ErrorBoundary } from "next/dist/client/components/error-boundary";
+
+function ErrorComponent({ error }: { error: Error }) {
+  return (
+    <div className="flex flex-col gap-2">
+      <h3>Error caught by boundary</h3>
+      <p>{error.message}</p>
+      <pre>{error.stack}</pre>
+    </div>
+  );
+}
 
 export function ClientLoaderComponent() {
   const [component, setComponent] = useState<JSX.Element | null>(null);
@@ -19,10 +30,12 @@ export function ClientLoaderComponent() {
   }
 
   return (
-    <div className="flex flex-col gap-2 border border-solid border-foreground p-4 rounded-2xl min-w-[500px]">
-      <h3>Load client component via server action</h3>
-      <Button onClick={handleClick}>test</Button>
-      {!component ? "waiting" : component}
-    </div>
+    <ErrorBoundary errorComponent={ErrorComponent}>
+      <div className="flex flex-col gap-2 border border-solid border-foreground p-4 rounded-2xl min-w-[500px]">
+        <h3>Load client component via server action</h3>
+        <Button onClick={handleClick}>test</Button>
+        {!component ? "waiting" : component}
+      </div>
+    </ErrorBoundary>
   );
 }
